f7becdc80a15f02f10001e880a6f16177f2b1c25
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-11-15 16:03:53 -0700

    halt when name is ruben

--- lib/sample_web/controllers/page_controller.ex
+++ lib/sample_web/controllers/page_controller.ex
@@ -1,21 +1,24 @@
 defmodule SampleWeb.PageController do
   use SampleWeb, :controller
+  plug :modify_params
+  plug :halt_on_ruben
+
   def index(conn, _params) do
     render conn, "index.html"
   end
 
-  def upload(conn, params) do
-    file = params["index"]["file"]
-    # Requires to have clamavdscann to work
-    case Clamxir.safe?(%Clamxir{daemonize: true}, file.path) do
-      true -> 
-        # Process the file and ... 
-        conn
-        |> put_flash(:info,  "Created successfully")
-        |> redirect(to: "/") 
-      false -> conn
-           |> put_flash(:error,  "Virus!!")
-           |> redirect(to: "/") 
+  defp modify_params(conn, _params) do
+    name = conn.params["name"] || " World"
+    conn
+    |> assign(:name, name)
+  end
+
+  defp halt_on_ruben(conn, _params) do
+    if(conn.params["name"] == "ruben") do
+      conn
+      |> send_resp(404, "not found")
+    else
+      conn
     end
   end
 end
