0f45ec34bd396e7364a7112a85e29aa925af92db
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-11-14 22:15:37 -0700

    As plug function.

--- lib/sample_web/controllers/page_controller.ex
+++ lib/sample_web/controllers/page_controller.ex
@@ -1,21 +1,32 @@
 defmodule SampleWeb.PageController do
   use SampleWeb, :controller
+  plug :check_virus when action in [:upload]
+
   def index(conn, _params) do
     render conn, "index.html"
   end
 
   def upload(conn, params) do
-    file = params["index"]["file"]
-    # Requires to have clamavdscann to work
+    #process file
+    conn
+    |> put_flash(:info,  "Created successfully")
+    |> redirect(to: "/")
+  end
+
+  defp check_virus(conn, _params) do
+    file = conn.params["index"]["file"]
     case Clamxir.safe?(%Clamxir{daemonize: true}, file.path) do
-      true -> 
-        # Process the file and ... 
+      true ->
+        # Process the file and ...
         conn
         |> put_flash(:info,  "Created successfully")
-        |> redirect(to: "/") 
-      false -> conn
-           |> put_flash(:error,  "Virus!!")
-           |> redirect(to: "/") 
+        |> redirect(to: "/")
+      false ->
+        conn
+        |> put_status(503)
+        |> put_flash(:error, "virus!!")
+        |> render("index.html")
+        |> Plug.Conn.halt()
     end
   end
 end
